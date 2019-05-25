009490a6917e88482f0ab5a6238be08e52baae8f
Jan-Strum <janusbased@gmail.com>
2019-05-25 14:09:51 -0500

    findOrCreateRecipe, front-end.

--- client/screens/Recipes.js
+++ client/screens/Recipes.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { View, FlatList, Text, StyleSheet } from 'react-native'
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  Button,
+  StyleSheet
+} from 'react-native'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import SingleRecipe from '../components/SingleRecipe'
 import SortRecipes from '../components/SortRecipes'
+import { TextInput } from 'react-native-gesture-handler'
 
 const FIND_ALL_RECIPES = gql`
   query findAllRecipes {
@@ -15,12 +23,23 @@ const FIND_ALL_RECIPES = gql`
   }
 `
 
+const FIND_OR_CREATE_RECIPE = gql`
+  mutation findOrCreateRecipe($name: String!) {
+    findOrCreateRecipe(name: $name) {
+      id
+      name
+    }
+  }
+`
+
 export default class Recipes extends React.Component {
   constructor() {
     super()
     this.state = {
       sortDisplay: false,
-      selectedField: ''
+      selectedField: '',
+      newRecipeDropdown: false,
+      name: ''
     }
   }
 
@@ -28,13 +47,17 @@ export default class Recipes extends React.Component {
     title: 'Recipes'
   }
 
-  toggleSortDropdown = () => {
-    this.setState({ sortDisplay: !this.state.sortDisplay })
-  }
-
   keyExtractor = item => item.id
 
   renderItem = ({ item }) => <SingleRecipe props={item} />
+
+  // toggleSortDropdown = () => {
+  //   this.setState({ sortDisplay: !this.state.sortDisplay })
+  // }
+
+  addRecipe = () => {
+    this.setState({ newRecipeDropdown: !this.state.newRecipeDropdown })
+  }
 
   render() {
     return (
@@ -47,6 +70,48 @@ export default class Recipes extends React.Component {
           }
           return (
             <View style={styles.container}>
+              <View>
+                <TouchableOpacity onPress={this.addRecipe}>
+                  {!this.state.newRecipeDropdown ? (
+                    <Text style={styles.button}>&#43;</Text>
+                  ) : (
+                    <Text style={styles.button}>&#8722;</Text>
+                  )}
+                </TouchableOpacity>
+                {this.state.newRecipeDropdown ? (
+                  <View style={styles.form}>
+                    <Text style={styles.label}>Name:</Text>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between'
+                      }}
+                    >
+                      <View>
+                        <TextInput
+                          placeholder='Enter the recipe name here...'
+                          onChangeText={name => this.setState({ name })}
+                          value={this.state.name}
+                        />
+                      </View>
+                      <View>
+                        <TouchableOpacity
+                          onPress={() => this.submitRecipe(this.state.name)}
+                          color='black'
+                        >
+                          <Text
+                            style={{
+                              color: this.state.name ? 'black' : '#C7C7CD'
+                            }}
+                          >
+                            Submit
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
+                ) : null}
+              </View>
               {/* <SortRecipes /> */}
               <FlatList
                 data={data.findAllRecipes}
@@ -64,5 +129,21 @@ export default class Recipes extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  button: {
+    textAlign: 'right',
+    paddingRight: 25,
+    fontSize: 35,
+    fontWeight: '200'
+  },
+  form: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  label: {
+    fontSize: 18,
+    paddingVertical: 10
   }
 })
