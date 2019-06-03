a730cbca391caa5b043ca7b60ea3e7847b4e9a52
Jan-Strum <janusbased@gmail.com>
2019-06-03 11:25:31 -0500

    Refactored styling into a separate directory.

--- client/components/AddIngredient.js
+++ client/components/AddIngredient.js
@@ -1,10 +1,11 @@
 /* eslint-disable nonblock-statement-body-position */
 /* eslint-disable react/no-unused-state */
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import StringFields from './form/StringFields'
 import HydrationField from './form/HydrationField'
 import ComplexityField from './form/ComplexityField'
+import { styles } from '../styles/form'
 import { Mutation } from 'react-apollo'
 import { CREATE_INGREDIENT } from '../gql/mutations'
 import { FIND_FULL_RECIPE } from '../gql/queries'
@@ -111,55 +112,3 @@ export default class AddIngredient extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  header: {
-    marginTop: 22,
-    marginBottom: 8,
-    fontSize: 20
-  },
-  form: {
-    marginHorizontal: 20
-  },
-  field: {
-    marginVertical: 5,
-    marginHorizontal: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  label: {
-    fontWeight: '500'
-  },
-  text: {
-    marginTop: 3,
-    marginBottom: 7
-  },
-  buttonContainer: {
-    flexDirection: 'row'
-  },
-  button: {
-    marginBottom: 7,
-    marginRight: 5,
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-    backgroundColor: '#F0F0F0',
-    borderRadius: 7
-  },
-  selectedButton: {
-    backgroundColor: '#C8C8C8'
-  },
-  selectedButtonText: {
-    color: 'white'
-  },
-  percentage: {
-    flexDirection: 'row',
-    paddingVertical: 7
-  },
-  percentageInput: {
-    marginLeft: 10
-  },
-  add: {
-    marginTop: 10,
-    alignItems: 'flex-end'
-  }
-})
--- client/components/form/ComplexityField.js
+++ client/components/form/ComplexityField.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { styles } from '../../styles/form'
 
 export default class ComplexityFIeld extends React.Component {
   constructor() {
@@ -43,55 +44,3 @@ export default class ComplexityFIeld extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  header: {
-    marginTop: 22,
-    marginBottom: 8,
-    fontSize: 20
-  },
-  form: {
-    marginHorizontal: 20
-  },
-  field: {
-    marginVertical: 5,
-    marginHorizontal: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  label: {
-    fontWeight: '500'
-  },
-  text: {
-    marginTop: 3,
-    marginBottom: 7
-  },
-  buttonContainer: {
-    flexDirection: 'row'
-  },
-  button: {
-    marginBottom: 7,
-    marginRight: 5,
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-    backgroundColor: '#F0F0F0',
-    borderRadius: 7
-  },
-  selectedButton: {
-    backgroundColor: '#C8C8C8'
-  },
-  selectedButtonText: {
-    color: 'white'
-  },
-  percentage: {
-    flexDirection: 'row',
-    paddingVertical: 7
-  },
-  percentageInput: {
-    marginLeft: 10
-  },
-  add: {
-    marginTop: 10,
-    alignItems: 'flex-end'
-  }
-})
