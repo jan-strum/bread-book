91680069a4b25439277e4887372e1018d3531de6
Jan-Strum <janusbased@gmail.com>
2019-06-11 14:16:16 -0500

    Cleaning up.

--- client/components/AddRecipe.js
+++ client/components/AddRecipe.js
@@ -1,28 +1,7 @@
 import React from 'react'
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet
-} from 'react-native'
-import { Mutation } from 'react-apollo'
-import { FIND_ALL_RECIPES } from '../gql/queries'
-import { FIND_OR_CREATE_RECIPE } from '../gql/mutations'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default class AddRecipe extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      newRecipeDropdown: false,
-      name: ''
-    }
-  }
-
-  addRecipe = () => {
-    this.setState({ newRecipeDropdown: !this.state.newRecipeDropdown })
-  }
-
   render() {
     const { navigation, message } = this.props
     return (
@@ -33,7 +12,7 @@ export default class AddRecipe extends React.Component {
             message === 'Create your first recipe' && styles.createFirst
           ]}
         >
-          {!this.state.newRecipeDropdown ? message : 'Cancel'}
+          {message}
         </Text>
       </TouchableOpacity>
     )
@@ -41,17 +20,6 @@ export default class AddRecipe extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  form: {
-    paddingBottom: 15,
-    paddingHorizontal: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: '400',
-    paddingBottom: 10
-  },
   add: {
     textAlign: 'right',
     marginTop: 20,
